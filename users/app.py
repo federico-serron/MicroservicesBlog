d2ec231d756b697551c0bc8a88395aae2f739947
fede.su <f.serron.umpierrez@gmail.com>
2022-08-25 18:00:27 +0200

    Working on endpoints

--- users/app.py
+++ users/app.py
@@ -1,3 +1,4 @@
+import json
 from msilib.schema import Error
 import time
 from flask import jsonify, request
@@ -15,80 +16,125 @@ loginManager.init_app(app)
 loginManager.login_view = 'login'
 @loginManager.user_loader
 def load_user(user_id):
-    return Users.query.get(int(user_id))
+	return Users.query.get(int(user_id))
 
+
+user_info_required = ['Name','Country','Age', 'Email','Username','Password','Role_id']
 
 
 @app.route('/')
 def info():
-    info = {
-        'Enviroment': 'Testing',
-        'Version': '1.0',
-        'Author': 'Federico Serron'
-    }
-    return jsonify(info)
+	info = {
+		'Enviroment': 'Testing',
+		'Version': '1.0',
+		'Author': 'Federico Serron'
+	}
+	return jsonify(info)
 
 
 
 @app.route('/signup', methods=['POST'])
 def signup():
-    user_data = request.get_json()
-    if 'username' not in user_data:
-        return 'Username/password are required', 412
-    elif 'password' not in user_data:
-        return 'Username/password are required', 412
-    else:
-        try:
-            new_signup = Users(username = user_data['username'], password = user_data['password'])
-            db.session.add(new_signup)
-            db.session.commit()
-        except Exception:
-            return 'There was an error, please try again', 412 
-        
-        return 'Successfully registered', 200
-    
-    
-    
+	user_data = request.get_json()
+	for param in user_info_required:
+		if param.lower() not in user_data:
+			return f'{param} required', 412 
+	else:
+		try:
+			new_signup = Users(name=user_data['name'],
+							   country=user_data['country'],
+							   age=user_data['age'],
+							   email=user_data['email'],
+							   username = user_data['username'],
+							   password = user_data['password'],
+							   role_id=user_data['role_id'])
+			
+			db.session.add(new_signup)
+			db.session.commit()
+		except Exception:
+			return 'There was an error, please try again', 412 
+		
+		return 'Successfully registered', 200
+	
+	
+	
 @app.route('/login', methods=['POST'])
 def login():
-    user_data = request.get_json()
-    if 'username' not in user_data:
-        return 'Username required', 412
-    elif 'password' not in user_data:
-        return 'Password required', 412
-    else:
-        try:
-            # Cheking out if exists an user with this username
-            user = Users.query.filter_by(username = user_data['username']).first()
-            # if it exists then check the password
-            if user:
-                if user.password == user_data['password']:
-                    login_user(user)
-                    return "Logged in succefully!", 200
-                else:
-                    return 'Username and/or Password invalid, please try again', 412
-        except Exception:
-            return 'There was an error, please contact admin', 500
-            
+	user_data = request.get_json()
+	if 'username' not in user_data:
+		return 'Username required', 412
+	elif 'password' not in user_data:
+		return 'Password required', 412
+	else:
+		try:
+			# Cheking out if exists an user with this username
+			user = Users.query.filter_by(username = user_data['username']).first()
+			# if it exists then check the password
+			if user:
+				if user.password == user_data['password']:
+					login_user(user)
+					return "Logged in succefully!", 200
+				else:
+					return 'Username and/or Password invalid, please try again', 412
+		except Exception:
+			return 'There was an error, please contact admin', 500
+			
 
 
 @app.route('/logout')
 @login_required
 def logout():
-    logout_user()
-    return "Successfully logged out", 200
+	logout_user()
+	return "Successfully logged out!", 200
+
+
+@app.route('/users')
+@login_required
+def list_users():
+	users = Users.query.all()
+	return jsonify([{
+		'Name': user.name,
+		'Email': user.email,
+		'Username': user.username,
+		'Country': user.country,
+		'Age': user.age,
+		'Role': user.role_id
+	} for user in users])
+	
+ 
+	
+@app.route('/users/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+@login_required
+def list_user(id):
+	user = Users.query.filter_by(id=id).first_or_404(description='There is no data with that specific information')
+	if user and request.method == 'GET': 
+		return jsonify({
+			'Name': user.name,
+			'Email': user.email,
+			'Username': user.username,
+			'Country': user.country,
+			'Age': user.age,
+			'Role': user.role_id
+		}), 200
+	elif user and request.method == 'PUT':
+		new_info = request.get_json()
+		user.username = new_info['username']
+		db.session.commit()
+		return 'Succefuly modified', 200
+	return 'There was an error', 500
+
 
 
 if __name__ == '__main__':
-    dbstatus = False
-    while dbstatus == False:
-        try:
-            db.create_all()
-        except:
-            time.sleep(2)
-        else:
-            dbstatus = True
-            
-    app.run(debug=True, port=5000, host='0.0.0.0')
-    
-    
+	dbstatus = False
+	while dbstatus == False:
+		try:
+			db.create_all()
+		except:
+			time.sleep(2)
+		else:
+			dbstatus = True
+			
+	app.run(debug=True, port=5000, host='0.0.0.0')
+	
+	
--- users/models.py
+++ users/models.py
@@ -1,12 +1,24 @@
+from unicodedata import name
 from config import db
 from flask_login import UserMixin
 
 
 class Users(UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50))
-    password = db.Column(db.String(20))
+    name = db.Column(db.String(25), nullable=False)
+    country = db.Column(db.String(25), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    email = db.Column(db.String(32), nullable = False)
+    username = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(20), nullable=False)
+    role_id = db.Column(db.Integer, default=2)
 
-    def __init__(self, username, password):
+    def __init__(self,name, country, age, email, username, password, role_id):
+        self.name = name
+        self.country = country
+        self.age = age
+        self.email = email
         self.username = username
         self.password = password
+        self.role_id = role_id
+        
